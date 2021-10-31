e17062ad5235f73c890dcec8007163302f68fd4e
KJM99 <2098kjm@naver.com>
2021-10-31 20:06:15 +0900

    setting 구현
    -테마 기능 추가
    -푸쉬 알림 기능 추가
    ** 네비게이션 바가 뜨지 않음

--- app/src/main/java/com/example/couchpotatosplan/setting/SettingFragment.java
+++ app/src/main/java/com/example/couchpotatosplan/setting/SettingFragment.java
@@ -1,22 +1,53 @@
 package com.example.couchpotatosplan.setting;
 
+import static android.content.Context.ALARM_SERVICE;
+import static androidx.core.content.ContextCompat.getSystemService;
+
+import android.annotation.SuppressLint;
+import android.app.AlarmManager;
+import android.app.AlertDialog;
+import android.app.PendingIntent;
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Switch;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
+import androidx.annotation.ColorInt;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import com.example.couchpotatosplan.R;
+import com.example.couchpotatosplan.myday.FragmentDialog;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.text.DateFormat;
+import java.util.Calendar;
 
 
 public class SettingFragment extends Fragment {
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.setting_fragment, container, false);
+
+        View view = inflater.inflate(R.layout.setting_fragment, container, false);
+
+        Intent intent = new Intent(getActivity(), SettingAlarm.class);
+        startActivity(intent);
+
+        return view;
     }
+
 }
