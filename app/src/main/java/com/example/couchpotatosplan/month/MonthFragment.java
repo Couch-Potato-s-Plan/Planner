256030819e3a68f5896df073b427c777df6855f5
Nominsol <xmxmxm3577@naver.com>
2021-11-21 15:09:32 +0900

    <해결> -00시 입력 안되는 오류 -리롤 버튼 이미지 삽입 -테마 기능 선택후 앱을 끄거나 화면 전환후 라디오 버튼 풀리는 문제

---
+++ app/src/main/java/com/example/couchpotatosplan/month/MonthFragment.java
@@ -0,0 +1,41 @@
+package com.example.couchpotatosplan.month;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.couchpotatosplan.R;
+
+public class MonthFragment extends Fragment {
+    public TextView exclude_btn;
+    public TextView fix_btn;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.month_fragment, container, false);
+
+        exclude_btn = view.findViewById(R.id.exclude);
+        fix_btn = view.findViewById(R.id.fix);
+
+        exclude_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new MonthExcludeFragment()).commit();
+            }
+        });
+
+        fix_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new MonthFixFragment()).commit();
+            }
+        });
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/couchpotatosplan/myday/MyDayFragment.java
@@ -0,0 +1,101 @@
+package com.example.couchpotatosplan.myday;
+
+import static com.example.couchpotatosplan.weekly.CalendarUtils.formattedDate;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.couchpotatosplan.R;
+import com.example.couchpotatosplan.month.MonthFixFragment;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+
+
+public class MyDayFragment extends Fragment {
+
+
+    private long mNow;
+    private Date mDate;
+    private SimpleDateFormat mFormat;
+    private ListView eventListView;
+    private MyDayEventAdapter adapter;
+    private ImageButton add_btn;
+    private ImageButton reroll_btn;
+
+    private FragmentDialog dialog;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.myday_fragment, container, false);
+
+        TextView mTextView = view.findViewById(R.id.todayDate);
+        add_btn = (ImageButton) view.findViewById(R.id.add_btn);
+        reroll_btn = (ImageButton) view.findViewById(R.id.reRoll_btn);
+        eventListView = view.findViewById(R.id.eventListView);
+
+        mFormat = new SimpleDateFormat("yyyy.MM.dd");
+
+        mTextView.setText(getTime());
+        addEventAction();
+
+        setEventAdpater();
+
+        return view;
+    }
+
+    private String getTime(){
+        mNow = System.currentTimeMillis();
+        mDate = new Date(mNow);
+        return mFormat.format(mDate);
+    }
+
+    private void addEventAction() {
+        add_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog = new FragmentDialog();
+                dialog.show(getActivity().getSupportFragmentManager(), "dialog");
+            }
+        });
+        reroll_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("MyLog", "reroll");
+                MyDayEventList.reroll(formattedDate(LocalDate.now()));
+                adapter.notifyDataSetChanged();
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, new MyDayFragment()).commit();
+            }
+        });
+        FragmentTransaction ft = getFragmentManager().beginTransaction(); ft.detach(this). attach(this).commit();
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        setEventAdpater();
+    }
+
+    public void setEventAdpater()
+    {
+        ArrayList<MyDayEvent> dailyEvents = MyDayEventList.eventsForDate(formattedDate(LocalDate.now()));
+        adapter = new MyDayEventAdapter(getActivity().getApplicationContext(), dailyEvents);
+        eventListView.setAdapter(adapter);
+    }
+
+
+}
